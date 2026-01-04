1f961c94d6e35e4d2c4e2faf3cd57a0ad9253dd9
shrutikesharwani1806-star <shrutikesharwani1806@gmail.com>
2026-01-04 17:28:38 +0530

    forth commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 function bookRoom(roomName, pricePerNight, roomImage) {
-    localStorage.setItem("roomName", roomName);
-    localStorage.setItem("roomPrice", pricePerNight);
-    localStorage.setItem("roomImage", roomImage);
-    window.location.href = "booking.html";
+  localStorage.setItem("roomName", roomName);
+  localStorage.setItem("roomPrice", pricePerNight);
+  localStorage.setItem("roomImage", roomImage);
+  window.location.href = "booking.html";
 }
 
 
@@ -24,3 +24,27 @@ if (el) {
   el.innerHTML = "";
   window.addEventListener("load", typeEffect);
 }
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        imperial: ['Imperial', 'serif'], // now font-imperial works
+      },
+    },
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'mea-culpa': ['"Mea Culpa"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
+
